e0084043be59dec690a6ccb41d5f5a60d152e696
Ryan Proffitt <ryan.d.proffitt@gmail.com>
2021-06-02 21:45:25 +0900

    oops, binary search is not a sort algogit add .!

---
+++ searching/test_search_algorithms.py
@@ -0,0 +1,39 @@
+# Ryan Proffitt
+# 29 May 2021
+# This file contains code to test the search algorithms.
+
+from random import randint
+
+from search_algorithms import binary_search
+
+def test_binary_search():
+    print("TESTING: binary_search()")
+
+    # Test random arrays a given number of times
+    for i in range(100):
+        # Create a list of random integers
+        list_len = 1024
+        my_list = [randint(0, list_len) for i in range(list_len)]
+
+        # Create an arbitrary value to search for and insert into list
+        arb_val = randint(0, list_len)
+        my_list[randint(0, list_len)] = arb_val
+
+        # Sort the list
+        my_list.sort()
+
+        # Search the list for the arbitrary integer
+        resulting_idx = binary_search(my_list, arb_val)
+
+        assert my_list[resulting_idx] == arb_val
+
+    # Test specific values at known positions
+    my_list = [0, 1, 1, 2, 4, 4, 4, 4, 8, 9, 10, 15, 16, 19, 20]
+    assert binary_search(my_list, 0) == 0
+    assert binary_search(my_list, 20) == 14
+    assert binary_search(my_list, 4) == 7
+    assert binary_search(my_list, 8) == 8
+
+    print("PASSED: binary_search()")
+
+# test_binary_search()
